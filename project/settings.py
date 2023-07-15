f11073623b6a938c494c9b4a7c3c9458cbd2946b
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-07-15 22:43:17 +0100

    added gunicorn and procfile

--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#rgu*q+yqgxy8vp+m&i*oa85r6v#u^51yvf%kr@o76nn%bd(lx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,10 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
