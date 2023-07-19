22717ffc31ad3c90c13fa1c04f99041d863994a8
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-07-19 14:34:41 +0100

    collected all static files into staticfiles

--- myproject/views.py
+++ myproject/views.py
@@ -9,9 +9,17 @@ from myproject.models import Portfolio
 def portfolio(request):
     
     
-    projects = Portfolio.objects.all() # select * from post
+    projects = Portfolio.objects.all() # select * from projects
     context = {
         "projects": projects
     }
-    
     return render(request, 'portfolio.html', context)
+
+
+
+def project_detail(request, project_id):
+    projects = Portfolio.objects.get(pk=project_id)
+    
+    
+    context = {"projects": projects}
+    return render(request, "project_detail.html",context)
