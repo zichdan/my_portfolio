4f1ad6d0ad40ef39c5c2bd6d6ab6f0e8e0cd4094
christain chidi okeke <chidiokeken@gmail.com>
2023-07-22 02:21:56 +0100

    commented  whitenoise storage and uncommenting the database of render

--- project/settings.py
+++ project/settings.py
@@ -104,10 +104,10 @@ DATABASES = {
 }
 
 
-# render postgresql database setup
-database_url = os.environ.get('DATABASE_URL')
+# # render postgresql database setup
+# database_url = os.environ.get('DATABASE_URL')
 
-DATABASES['default'] = dj_database_url.parse(database_url)
+# DATABASES['default'] = dj_database_url.parse(database_url)
 
     
     
@@ -164,7 +164,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
