fb959b3495220385596669e07c0ad5778b1b2105
OKORIE DANIEL EZICHI <zichdan1999@gmail.com>
2023-07-25 03:30:42 +0100

    corrected the misplacing of staticfiles and static root

--- project/settings.py
+++ project/settings.py
@@ -176,11 +176,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = '/media/'
